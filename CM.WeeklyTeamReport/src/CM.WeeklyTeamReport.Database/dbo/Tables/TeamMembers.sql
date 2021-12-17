fc0290aa64847763c99db1596245d353940efa8d
Ilya <goneron@mail.ru>
2021-12-17 22:43:37 +0700

    Fix: refactoring database

--- CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Database/dbo/Tables/TeamMembers.sql
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Database/dbo/Tables/TeamMembers.sql
@@ -2,12 +2,13 @@
     [TeamMemberId] INT            IDENTITY (1, 1) NOT NULL,
     [FirstName]    NVARCHAR (20)  NOT NULL,
     [LastName]     NVARCHAR (20)  NOT NULL,
-    [Title]        NVARCHAR (20)  NULL,
-    [InviteLink]   NVARCHAR (200) NULL,
+    [Title]        NVARCHAR (100) NULL,
     [Mail]         NVARCHAR (100) NOT NULL,
+    [Subject]      NVARCHAR (MAX) NOT NULL,
     [CompanyId]    INT            NOT NULL,
     CONSTRAINT [PK_TeamMembers] PRIMARY KEY CLUSTERED ([TeamMemberId] ASC),
-    CONSTRAINT [FK_TeamMembers_Companies] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([CompanyId]),
-    CONSTRAINT [IX_TeamMembers_Mail] UNIQUE NONCLUSTERED ([Mail] ASC)
+    CONSTRAINT [FK_TeamMembers_Companies] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([CompanyId])
 );
+
+
 
