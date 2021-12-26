cf01d2a64b2ca5a93850b76d8b5b32d0ac6cb5d5
Xander11rgn <xander11rgn-cool@yandex.ru>
2021-12-26 07:47:02 +0300

    feat: added email service controller

---
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.WebApp/Controllers/EmailServiceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using CM.WeeklyTeamReport.WebApp.Email;
+using System;
+using Newtonsoft.Json;
+
+namespace CM.WeeklyTeamReport.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/invite")]
+    public class EmailServiceController : ControllerBase
+    {
+        [ExcludeFromCodeCoverage]
+        [ActivatorUtilitiesConstructor]
+        public EmailServiceController()
+        {
+        }
+
+        [HttpPost]
+        public async void SendInvite([FromBody] EmailSet emailData)
+        {
+            EmailService emailService = new();
+            await emailService.SendEmailAsync(emailData);
+            //return RedirectToAction("Index");
+        }
+
+    }
+
+    public class EmailSet
+    {
+        public string Email { get; set; }
+        public string InviteLink { get; set; }
+        public string UserName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.WebApp/Email/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+using CM.WeeklyTeamReport.WebApp.Controllers;
+
+namespace CM.WeeklyTeamReport.WebApp.Email
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(EmailSet emailData)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("noreply@anko", "noreply.testanko@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", emailData.Email));
+            emailMessage.Subject = "Invitation to " + emailData.CompanyName;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = "Hello, <b>" + emailData.UserName + "!</b> Join us by clicking the following link:<br>" + emailData.InviteLink + "!"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 465, true);
+                await client.AuthenticateAsync("noreply.testanko@gmail.com", "hjltryhsaskjperu");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
